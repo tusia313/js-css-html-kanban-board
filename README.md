37e00a3f24a9a0efd3aab71c959382150fe24fc2
Marta Łoskot <loskotmarta@gmail.com>
2024-03-18 21:07:21 +0100

    Task data

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # js-css-html-kanban-board
-"To-do list, but developed, help me practicing methods/properties of nodes and drag events. :)
+"To-do list, but developed, help me practicing OOP (finally!), methods/properties of nodes and drag events. :)
--- js/app.js
+++ js/app.js
@@ -1,2 +1,25 @@
 const taskLists = document.querySelectorAll('.task-list')
 const backlogTasks = document.querySelector('#backlog .task-list')
+
+let tasks = [
+    {
+        id : 0,
+        title : "Fix submit button",
+        description : "The submit button stop working, since the last release."
+    },
+    {
+        id : 1,
+        title : "Change text in C and T's",
+        description : "Needs to be updating as per the i m p o r t a n t buisness meeting."
+    },
+    {
+        id : 2,
+        title : "Change banner picture",
+        description : "Marketing has requested a new banner to be added for the website."
+    },
+]
+
+function createTask() {
+    const taskCard = document.createElement('div')
+    taskCard.classList.add('task-container')
+}
